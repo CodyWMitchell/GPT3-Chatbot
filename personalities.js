55eb5d5657c4792b08000558a9f84ddd36642511
Cody Mitchell <comitche@redhat.com>
2022-05-23 15:15:21 -0400

    Update personalities and increase max tokens

--- personalities.js
+++ personalities.js
@@ -5,7 +5,9 @@ const personalities = [
         description: "A chatbot that responds like Lady Gaga",
         prePrompt: "Respond to the following messages like Lady Gaga.\nMessage: ",
         postPrompt: "\nResponse: ",
-        aiParams: {} // additional params passed to openai
+        aiParams: {
+            max_tokens: 32
+        } // additional params passed to openai
     },
     {
         id: "git-bot",
@@ -13,7 +15,9 @@ const personalities = [
         description: "A chatbot that recommends git commands",
         prePrompt: "Respond to the following messages with git commands.\nMessage: ",
         postPrompt: "\nResponse: ",
-        aiParams: {}
+        aiParams: {
+            max_tokens: 32
+        }
     },
     {
         id: "angry-bot",
@@ -21,7 +25,9 @@ const personalities = [
         description: "A chatbot that responds angrily!",
         prePrompt: "Respond to the following messages with angry responses.\nMessage: ",
         postPrompt: "\nResponse: ",
-        aiParams: {}
+        aiParams: {
+            max_tokens: 32
+        }
     },
     {
         id: "90s-bot",
@@ -29,7 +35,9 @@ const personalities = [
         description: "A chatbot that responds with slang from the 90s",
         prePrompt: "Respond to the following messages with slang from the 90s.\nMessage: ",
         postPrompt: "\nResponse: ",
-        aiParams: {}
+        aiParams: {
+            max_tokens: 32
+        }
     },
     {
         id: "hippie-bot",
@@ -37,8 +45,20 @@ const personalities = [
         description: "A chatbot that responds with 70's hippie slang",
         prePrompt: "Respond to the following messages with 70's hippie slang.\nMessage: ",
         postPrompt: "\nResponse: ",
-        aiParams: {}
-    }
+        aiParams: {
+            max_tokens: 32
+        }
+    },
+    {
+        id: "lonely-bot",
+        name: "Lonely Bot",
+        description: "A chatbot that responds with lonely responses",
+        prePrompt: "Respond to the following messages with lonely responses.\nMessage: ",
+        postPrompt: "\nResponse: ",
+        aiParams: {
+            max_tokens: 32
+        }
+    },
 ]
 
 exports.personalities = personalities;
